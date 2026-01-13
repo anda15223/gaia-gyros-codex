0f101c1a0ec3192c17e7b659dcab98c1ad2f0801
anda15223 <9xzbvxw28f@privaterelay.appleid.com>
2026-01-13 16:25:03 +0100

    Change server port from 5173 to 5179

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  root: fileURLToPath(new URL(".", import.meta.url)),
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5179,
     strictPort: true,
   },
 });
