3f64467c782f23a5d2c043ca71eb21eb9789f9be
anda15223 <9xzbvxw28f@privaterelay.appleid.com>
2026-01-13 16:20:14 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: fileURLToPath(new URL(".", import.meta.url)),
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+});
